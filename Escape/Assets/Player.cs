01afddaf0c08253a459deb0552fc69b7e1e212bb
DropBear25 <michelle25@live.co.uk>
2021-03-14 14:34:39 +0000

    yey its working again

---
+++ Escape/Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float currentVelocity;
+    public float MoveSpeed = 3f;
+    public float SmoothRotationTime = 0.25f;
+     
+
+    float CurrentSpeed;
+    float speedVelocity;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 inputDir = input.normalized;
+
+
+        if (inputDir != Vector2.zero)
+        {
+            float rotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, rotation, ref currentVelocity, SmoothRotationTime);
+        }
+        float targetSpeed = MoveSpeed * inputDir.magnitude;
+        CurrentSpeed = Mathf.SmoothDamp(CurrentSpeed, targetSpeed, ref speedVelocity, 0.1f);
+
+        transform.Translate(transform.forward * CurrentSpeed  * Time.deltaTime, Space.World);
+    }
+}
---
+++ Escape/Assets/PlayerCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+
+    public float Yaxis;
+    public float Xaxis;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Yaxis = Input.GetAxis("Mouse X");
+
+        print(Yaxis); 
+
+
+
+
+
+    }
+}
