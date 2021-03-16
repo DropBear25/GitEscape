bd57675fc573977414331a48a8ab682d4ab673fb
DropBear25 <michelle25@live.co.uk>
2021-03-16 16:18:34 +0000

    bullets

--- Escape/Assets/Gun.cs
+++ Escape/Assets/Gun.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
+    FixedButton Button;
+
+    [SerializeField]
+        GameObject m_bulletPrefab;
+
+
+    [SerializeField]
+    Transform m_bulletSpawnPoint;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if(m_bulletPrefab && m_bulletPrefab)
+            {
+                Instantiate(m_bulletPrefab, m_bulletSpawnPoint.position, m_bulletSpawnPoint.rotation * m_bulletPrefab.transform.rotation);
+            }
         
+        }
     }
 }
